0cf970d71658ca7c9cfd259dc7eefd6ea713a5a6
SimranKamboj320 <simrankamboj320@gmail.com>
2026-01-09 17:14:02 +0530

    Created UserRepository and GlobalExceptionHandler for exception handling

---
+++ src/main/java/com/jsp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.jsp.exception;
+
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> UserNotFoundException(UserNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> dataIntegrityViolationException(DataIntegrityViolationException d){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(d.getMessage());
+    }
+}
