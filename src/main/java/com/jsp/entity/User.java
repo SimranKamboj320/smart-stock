908f1f7644ec426f9749d60a2df0d0a5f40d7629
SimranKamboj320 <simrankamboj320@gmail.com>
2026-01-09 16:36:10 +0530

    initial commit added user entity and created folder structure

---
+++ src/main/java/com/jsp/entity/User.java
@@ -0,0 +1,26 @@
+package com.jsp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class User {
+    @Id
+    private int userId;
+    private String name;
+    private long phoneNo;
+    private String email;
+    private LocalDate dob;
+    private String gender;
+    private String address;
+    private String password;
+    private String role;
+}
